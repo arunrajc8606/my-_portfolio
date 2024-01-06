cf11aedcd408e90d447edc8d4883802519eb1acf
Arun c <arunrajc8606@gmail.com>
2024-01-06 17:23:40 +0530

    This is my portfolio updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -268,3 +268,31 @@
   new PureCounter();
 
 })()
+function sendmail() {
+  if(document.getElementById('name').value.length>0
+  &&document.getElementById('email').value.length>0
+  &&document.getElementById('message').value.length>0
+  &&document.getElementById('email').value.includes("@")){
+
+  var params={
+      name:document.getElementById('name').value,
+      email:document.getElementById('email').value,
+      message:document.getElementById('message').value
+
+  }
+  const serviceId="service_e2hl9ts"
+const templateId="template_v7a7fxw"
+emailjs.send(serviceId,templateId,params)
+.then(
+  res => {
+      document.getElementById('name').value="";
+      document.getElementById('email').value="";
+      document.getElementById('message').value="";
+      console.log(res);
+      alert('message send succesfully');
+  })
+.catch(err=>{console.log(err);})
+}else{
+  alert("please fill the form correctly")
+}
+}
